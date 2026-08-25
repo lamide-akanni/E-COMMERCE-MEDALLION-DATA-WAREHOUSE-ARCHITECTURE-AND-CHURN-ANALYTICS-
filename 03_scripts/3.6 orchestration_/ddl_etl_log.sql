5696eeca77d5bba564d9d9b492f782b4bad11e0b
OLAMIDE AKANNI <akannilmd@gmail.com>
2026-08-25 12:29:07 +0100

    Create etl_log table for ETL process logging

--- 03_scripts/3.6 orchestration_/ddl_etl_log.sql
+++ 03_scripts/3.6 orchestration_/ddl_etl_log.sql
@@ -1,1 +1,15 @@
+IF OBJECT_ID('dbo.etl_log', 'U') IS NOT NULL
+    DROP TABLE dbo.etl_log;
+GO
 
+CREATE TABLE dbo.etl_log (
+    log_id          INT IDENTITY(1,1) PRIMARY KEY,
+    run_id          UNIQUEIDENTIFIER,
+    pipeline_step   NVARCHAR(100),
+    start_time      DATETIME2,
+    end_time        DATETIME2,
+    duration_sec    INT,
+    status          NVARCHAR(20),
+    error_message   NVARCHAR(MAX) NULL
+);
+GO
