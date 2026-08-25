e0cc4e31793019c3b68c8146c37eb34762e5725f
OLAMIDE AKANNI <akannilmd@gmail.com>
2026-08-25 09:04:49 +0100

    Create and drop multiple bronze schema tables
    Added new tables for customer, product, sales details, and ERP data.

--- 03_scripts/3.1 setup/ddl_bronze.sql
+++ 03_scripts/3.1 setup/ddl_bronze.sql
@@ -3,7 +3,7 @@
 IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_cust_info;
 GO
-
+    
 CREATE TABLE bronze.crm_cust_info (
 cst_id                 INT, 
 cst_key                NVARCHAR(50),
@@ -15,10 +15,11 @@ cst_create_date        DATE
 );
 GO
 
+    
 IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_prd_info;
 GO
-
+    
 CREATE TABLE bronze.crm_prd_info(
 prd_id         INT, 
 prd_key        NVARCHAR(50),
@@ -30,8 +31,7 @@ prd_end_dt     DATETIME
 );
 GO
 
-
-
+    
 IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE bronze.crm_sales_details;
 GO
@@ -49,10 +49,11 @@ CREATE TABLE bronze.crm_sales_details(
 );
 GO
 
+    
 IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
     DROP TABLE bronze.erp_cust_az12;
 GO
-
+    
 CREATE TABLE bronze.erp_cust_az12 (
     cid    NVARCHAR(50),
     bdate  DATE,
@@ -60,6 +61,7 @@ CREATE TABLE bronze.erp_cust_az12 (
 );
 GO
 
+    
 IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
     DROP TABLE bronze.erp_loc_a101;
 GO
@@ -70,10 +72,11 @@ CREATE TABLE bronze.erp_loc_a101 (
 );
 GO
 
-
+    
 IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
     DROP TABLE bronze.erp_px_cat_g1v2;
 GO
+    
    CREATE TABLE bronze.erp_px_cat_g1v2 (
     id           NVARCHAR(50),
     cat          NVARCHAR(50),
