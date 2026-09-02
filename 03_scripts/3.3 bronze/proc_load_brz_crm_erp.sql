66392fbf4a61a8c1d8a8c303d21c99dba8523374
OLAMIDE AKANNI <akannilmd@gmail.com>
2026-09-02 12:31:19 +0100

    Update file paths for bulk inserts in SQL script

--- 03_scripts/3.3 bronze/proc_load_brz_crm_erp.sql
+++ 03_scripts/3.3 bronze/proc_load_brz_crm_erp.sql
@@ -18,7 +18,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.crm_cust_info;
         BULK INSERT bronze.crm_cust_info
-        FROM 'C:\Users\lamid\Documents\dbara\source_crm\cust_info.csv'
+        FROM 'C:\Users\lamid\Documents\db\source_crm\cust_info.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -32,7 +32,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.crm_prd_info;
         BULK INSERT bronze.crm_prd_info
-        FROM 'C:\Users\lamid\Documents\dbara\source_crm\prd_info.csv'
+        FROM 'C:\Users\lamid\Documents\db\source_crm\prd_info.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -46,7 +46,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.crm_sales_details;
         BULK INSERT bronze.crm_sales_details
-        FROM 'C:\Users\lamid\Documents\dbara\source_crm\sales_details.csv'
+        FROM 'C:\Users\lamid\Documents\db\source_crm\sales_details.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -64,7 +64,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.erp_cust_az12;
         BULK INSERT bronze.erp_cust_az12
-        FROM 'C:\Users\lamid\Documents\dbara\source_erp\cust_az12.csv'
+        FROM 'C:\Users\lamid\Documents\db\source_erp\cust_az12.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -78,7 +78,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.erp_loc_a101;
         BULK INSERT bronze.erp_loc_a101
-        FROM 'C:\Users\lamid\Documents\dbara\source_erp\loc_a101.csv'
+        FROM 'C:\Users\lamid\Documents\db\source_erp\loc_a101.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -92,7 +92,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.erp_px_cat_g1v2;
         BULK INSERT bronze.erp_px_cat_g1v2
-        FROM 'C:\Users\lamid\Documents\dbara\source_erp\px_cat_g1v2.csv'
+        FROM 'C:\Users\lamid\Documents\db\source_erp\px_cat_g1v2.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
